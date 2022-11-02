0ee84db65dc6ea0fd549ba880d51c4d91a33eeb5
Randy <117179176+rgriggs0072@users.noreply.github.com>
2022-11-02 14:13:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 
-':milky_way:' streamlit.text(':milky_way:')
+':milky_way:', streamlit.text(':milky_way:')
 streamlit.text(":heart:Omega 3 & Blueberry Oatmeal")
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
