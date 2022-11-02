878d178be26ede1b6efaa0f3ccd799660715741e
Randy <117179176+rgriggs0072@users.noreply.github.com>
2022-11-02 13:53:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text(':bowl_with_spoon:Omega 3 & Blueberry Oatmeal')
