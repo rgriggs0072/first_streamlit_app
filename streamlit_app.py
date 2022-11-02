30fc7d6ba22521f91d4be015fc9012d98f8096a8
Randy <117179176+rgriggs0072@users.noreply.github.com>
2022-11-02 14:56:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('✅ Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🍞🥑 Avocado Toast')
+
+streamlit.header('🍌🍓 Build your own fruit smoothie 🥝🍇')
