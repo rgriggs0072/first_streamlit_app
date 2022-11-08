054f1445e07562e1ee1dbb688cef6b6259e0c523
Randy <117179176+rgriggs0072@users.noreply.github.com>
2022-11-07 19:18:22 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,8 +80,8 @@ if streamlit.button('Get Fruit Load List'):
 streamlit.header("What fruit would you like to add?")
 pick_one = streamlit.text_input('pick one')
 streamlit.write('thanks for picking ', pick_one)
-get_from_function = insert_row_snowflake(pick_one)
-      streamlit.dataframe(back_from_function)
+    get_from_function = insert_row_snowflake(pick_one)
+    streamlit.dataframe(back_from_function)
 
 
 # add data to the fruit_load_list table in snowflake
