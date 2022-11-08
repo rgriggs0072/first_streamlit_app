5f688615a4b131eed28634abfb35f3f52df1e542
Randy <117179176+rgriggs0072@users.noreply.github.com>
2022-11-07 17:28:29 -0800

    Update streamlit_app.py
    add second text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,11 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+
+# New Section to display fruityvice api response
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
